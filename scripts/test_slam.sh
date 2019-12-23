c97d56b6b244a385901011588d107e0f12ac76e7
Daniela Becerra <madajiji@gmail.com>
2019-12-23 13:25:45 -0600

    feat: finish test_slam script

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-source /opt/ros/kinetic/setup.bash
-sleep 5
+pkill -f ros
+pkill -f rviz
+pkill -f gazebo
 cd ~/catkin_ws2
-roslaunch turtlebot_gazebo turtlebot_world.launch
+source devel/setup.bash
+sleep 5
+xterm -T "Turtlebot" -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -T "gmapping" -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm -T "rviz" -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -T "teleop" -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
